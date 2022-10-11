3d6c741428ca56f39c80d3ce26495017b4110d57
rezaaar <rabbs3245@gmail.com>
2022-10-11 14:09:21 +0700

    init commit

---
+++ src/components/GamestateFinish.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="modal">
+        <h1>Game over!</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
